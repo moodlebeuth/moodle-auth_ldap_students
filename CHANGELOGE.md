143be5511914e52372f4722d0cf5bb93c4f5807b
Michael Wuttke <mwuttke@beuth-hochschule.de>
2016-06-07 14:52:26 +0200

    inserted the plugin version number neue Datei:     CHANGELOGE.md geändert:       version.php

---
+++ CHANGELOGE.md
@@ -0,0 +1,5 @@
+CHANGELOG.md
+
+2016060701	made some improvements to run scheduled tasks
+
+2016060500	Initial version of auth_ldap_instances.
--- version.php
+++ version.php
@@ -4,5 +4,5 @@ require(dirname(dirname(__FILE__)).'/ldap_syncplus/version.php');
 
 $plugin->component = 'auth_ldap_students';
 $plugin->dependencies = ['auth_ldap_syncplus' => 2016032000];
-$version  = 20160607;
+$plugin->version  = 2016060701;
 
