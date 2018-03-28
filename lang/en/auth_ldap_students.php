c70ba1360d651348bcb8d6056c776427358b2d3c
Michael Wuttke <michael.wuttke@beuth-hochschule.de>
2018-03-28 17:29:03 +0200

    updated the language file

--- lang/en/auth_ldap_students.php
+++ lang/en/auth_ldap_students.php
@@ -25,3 +25,7 @@
 $string['pluginname'] = 'LDAP server (students)';
 $string['auth_ldap_studentsdescription'] = 'Clone instance of ldap_syncplus';
 $string['synctask'] = 'LDAP users sync job (students)';
+$string['removeuser_graceperiod'] = 'Fully deleting grace period';
+$string['removeuser_graceperiod_desc'] = 'After suspending a user internally, the synchronization script will wait for this number of days until the user will be fully deleted internal. If the user re-appears in LDAP within this grace period, the user will be reactivated. Note: This setting is only used if "Removed ext user" is set to "Suspend internal and fully delete internal after grace period"';
+$string['sync_script_createuser_enabled'] = 'If enabled (default), the synchronization script will create Moodle accounts for all LDAP users if they have never logged into Moodle before. If disabled, the synchronization script will not create Moodle accounts for all LDAP users.';
+$string['sync_script_createuser_enabled_key'] = 'Add new users';
